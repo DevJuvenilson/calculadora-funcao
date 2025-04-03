8d22dceafe63a2ca7f1b8e746510a2ec881d388b
Daniel Ribeiro <109773063+DevJuvenilson@users.noreply.github.com>
2025-04-02 21:55:54 -0300

    Setup of the project

---
+++ calculadora-funcao/src/pages/Home/index.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Calculadora</h1>
+        <div className="container">
+            <div className="conteudo-esquerdo">
+                
+            </div>
+            <div className="conteudo-direito">
+
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
